18029ba83e5df423130b00731130d8b42ad6410a
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-02-08 23:01:01 +0200

    Add project files.

---
+++ Session4 Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session4_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
