3024c6edd8ef5d4006d60cd2ed98477e29c215b2
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-02-09 14:26:46 +0200

    HashSet

---
+++ Session4 Advanced/Movie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session4_Advanced
+{
+    class MovieEqualityComparer : IEqualityComparer<Movie>
+    {
+        public bool Equals(Movie? x, Movie? y)
+        {
+            return x?.Price.Equals(y?.Price)??(y is null ? true : false);
+        }
+
+        public int GetHashCode([DisallowNull] Movie obj)
+        {
+            return obj.Price.GetHashCode();
+        }
+    }
+
+    internal class Movie : IEquatable<Movie>
+    {
+        public Movie(int iD, string title, decimal price)
+        {
+            ID = iD;
+            Title = title;
+            Price = price;
+        }
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public override string ToString()
+        {
+            return $"ID = {ID}, Title : {Title}, Price = {Price}";  
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID, Title, Price);
+        }
+        //public override bool Equals(object? obj)
+        //{
+        //    Movie? other = obj as Movie;
+        //    return this.ID.Equals(other?.ID) && this.Title.Equals(other?.Title) && this.Price.Equals(other?.Price);
+        //}
+
+        public bool Equals(Movie? other)
+        {
+            return this.ID.Equals(other?.ID) && this.Title.Equals(other?.Title) && this.Price.Equals(other?.Price);
+        }
+    }
+}
--- Session4 Advanced/Program.cs
+++ Session4 Advanced/Program.cs
@@ -20,18 +20,19 @@
             //    {"Omar",2 },
             //    {"Ali",3 }
             //};
+            ////
             if (!note.ContainsKey("Amir"))
                 note.Add("Amir", 4);
             else Console.WriteLine("Amir already found");
-
+            /////
             bool res1 = note.TryAdd("Amir", 4);
-
+            /////
             if (note.ContainsKey("AmirAli"))
                 Console.WriteLine(note["AmirAli"]);
-
+            /////
             bool res2 = note.TryGetValue("Amir", out int value);
             Console.WriteLine($"Try Result : {res2}, Retreived Value : {value}");
-
+            /////
             foreach(var item in note)
             {
                 Console.WriteLine($"Key : {item.Key}, Value : {item.Value}");
@@ -47,6 +48,31 @@
                 Console.WriteLine(item);
             }
 
+            #endregion
+
+            #region HashSet
+            HashSet<int> set = new HashSet<int>();
+            set.Add(1); //1
+            set.Add(2); //2
+            set.Add(3); //3
+            set.Add(4); //4
+            set.Add(4); //4 wasnt added
+            foreach(var item in set)
+            {
+                Console.WriteLine(item);
+            }
+            ///////
+            HashSet<Movie> movies = new HashSet<Movie>()
+            {
+                new Movie(1,"hhh",30),
+                new Movie(2,"ggg",40),
+                new Movie(3,"aaa",50)
+            };
+            movies.Add(new Movie(2, "ggg", 40));
+            foreach (var item in movies)
+            {
+                Console.WriteLine(item);
+            }
             #endregion
         }
     }
